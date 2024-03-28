154c7bb0c067010395c5e37c4b78bb892eeaa263
Rohit Kumar <rohit.espk@gmail.com>
2024-03-28 21:47:09 +0530

    All changes till 28th March

--- force-app/main/default/lwc/healthPocBarChart/healthPocBarChart.js
+++ force-app/main/default/lwc/healthPocBarChart/healthPocBarChart.js
@@ -1,11 +1,16 @@
 import { LightningElement, wire } from 'lwc';
-import getData from '@salesforce/apex/HealthPocChartsController.getData';
+import getBarChartData from '@salesforce/apex/HealthPocChartsController.getBarChartData';
+import getBarChartData1 from '@salesforce/apex/HealthPocChartsController.getBarChartData1';
+import getBarChartData2 from '@salesforce/apex/HealthPocChartsController.getBarChartData2';
+import getBarChartData3 from '@salesforce/apex/HealthPocChartsController.getBarChartData3';
 
-export default class Gen_opportunitychart extends LightningElement {
+
+export default class Gen_Barchart extends LightningElement {
     chartConfiguration;
-    doughnutChartConfiguration;
-    lineChartConfiguration;
-    treeMapChartConfiguration;
+    chartConfiguration1;
+    chartConfiguration2;
+    chartConfiguration3;
+
 
     colorFromRaw(ctx) {
         if (ctx.type !== 'data') {
@@ -19,8 +24,8 @@ export default class Gen_opportunitychart extends LightningElement {
             .rgbString();
     }
 
-    @wire(getData, {})
-    getData({ error, data }) {
+    @wire(getBarChartData, {})
+    getBarChartData({ error, data }) {
         if (error) {
             this.error = error;
             console.log('error => ' + JSON.stringify(error));
@@ -29,16 +34,91 @@ export default class Gen_opportunitychart extends LightningElement {
             let chartData = [];
             let chartLabels = [];
             if (data.ChartData) {
-                // alert(JSON.stringify(data.ChartData));
+                //alert(JSON.stringify(data.ChartData));
                 data.ChartData.forEach(opp => {
                     chartData.push(opp.chartData);
                     chartLabels.push(opp.chartLabel);
                 });
             }
 
-
+            // Bar Chart
             this.chartConfiguration = {
-                type: 'horizontalBar',
+                type: 'bar',
+                data: {
+                    labels: chartLabels,
+                    datasets: [
+                        {
+                            axis: 'y',
+                            label: data.barLabel,
+                            barPercentage: data.barPercentage,
+                            barThickness: data.barThickness,
+                            maxBarThickness: data.maxBarThickness,
+                            minBarLength: data.minBarLength,
+                            backgroundColor: data.backgroundColor,
+                            data: chartData,
+							borderWidth : 0
+                        },
+                    ],
+                },
+               options: {
+                   plugins: {
+                       title: {
+                           display: false
+                       }
+                   },
+                   plugins: {
+                       legend: {
+                           display: false
+                       }
+                   },
+                    indexAxis: 'y',
+                    scales: {
+                        x: {
+                            grid: {
+                                display: false, // Hide x-axis lines
+                            },
+                            border: {
+                                display: false // remove border lines
+                            }
+                        },
+                        y: {
+                           grid: {
+                                display: false, // Hide y-axis lines
+                            },
+                            border: {
+                                display: false // remove border lines
+                            }
+                        },
+                    },
+
+                },
+            };
+
+            console.log('data => ', data);
+            this.error = undefined;
+        }
+    }
+
+    @wire(getBarChartData1, {})
+    getBarChartData1({ error, data }) {
+        if (error) {
+            this.error = error;
+            console.log('error => ' + JSON.stringify(error));
+            this.chartConfiguration1 = undefined;
+        } else if (data) {
+            let chartData = [];
+            let chartLabels = [];
+            if (data.ChartData) {
+                //alert(JSON.stringify(data.ChartData));
+                data.ChartData.forEach(opp => {
+                    chartData.push(opp.chartData);
+                    chartLabels.push(opp.chartLabel);
+                });
+            }
+
+            // Bar Chart
+            this.chartConfiguration1 = {
+                type: 'bar',
                 data: {
                     labels: chartLabels,
                     datasets: [
@@ -55,14 +135,199 @@ export default class Gen_opportunitychart extends LightningElement {
                     ],
                 },
                 options: {
+                    plugins: {
+                       title: {
+                           display: false
+                       }
+                   },
+                   plugins: {
+                       legend: {
+                           display: false
+                       }
+                   },
                     indexAxis: 'y',
-                }
+                    scales: {
+                        x: {
+                            grid: {
+                                display: false, // Hide x-axis lines
+                            },
+                            border: {
+                                display: false // remove border lines
+                            }
+                        },
+                        y: {
+                           grid: {
+                                display: false, // Hide y-axis lines
+                            },
+                            border: {
+                                display: false // remove border lines
+                            }
+                        },
+                    },
+
+                },
             };
-
-
 
             console.log('data => ', data);
             this.error = undefined;
+        }
+    }
+
+    @wire(getBarChartData2, {})
+    getBarChartData2({ error, data }) {
+        if (error) {
+            this.error = error;
+            console.log('error => ' + JSON.stringify(error));
+            this.chartConfiguration2 = undefined;
+        } else if (data) {
+            let chartData = [];
+            let chartLabels = [];
+            if (data.ChartData) {
+                //alert(JSON.stringify(data.ChartData));
+                data.ChartData.forEach(opp => {
+                    chartData.push(opp.chartData);
+                    chartLabels.push(opp.chartLabel);
+                });
+            }
+
+            // Bar Chart
+            this.chartConfiguration2 = {
+                type: 'bar',
+                data: {
+                    labels: chartLabels,
+                    
+                    datasets: [
+                        {
+                            axis: 'y',
+                            label: data.barLabel,
+                            barPercentage: data.barPercentage,
+                            barThickness: data.barThickness,
+                            maxBarThickness: data.maxBarThickness,
+                            minBarLength: data.minBarLength,
+                            backgroundColor: data.backgroundColor,
+                            data: chartData,
+                        },
+                    ],
+                },
+                options: {
+                    plugins: {
+                       title: {
+                           display: false
+                       }
+                   },
+                   plugins: {
+                       legend: {
+                           display: false
+                       }
+                   },
+                    indexAxis: 'y',
+                    scales: {
+                        x: {
+                            grid: {
+                                display: false, // Hide x-axis lines
+                            },
+                            border: {
+                                display: false // remove border lines
+                            }
+                        },
+                        y: {
+                           grid: {
+                                display: false, // Hide y-axis lines
+                            },
+                            border: {
+                                display: false // remove border lines
+                            }
+                        },
+                    },
+
+                },
+            };
+
+            console.log('data => ', data);
+            this.error = undefined;
+        }
+    }
+
+    @wire(getBarChartData3, {})
+    getBarChartData3({ error, data }) {
+        if (error) {
+            this.error = error;
+            console.log('error => ' + JSON.stringify(error));
+            this.chartConfiguration3 = undefined;
+        } else if (data) {
+            let chartData = [];
+            let chartLabels = [];
+            if (data.ChartData) {
+                //alert(JSON.stringify(data.ChartData));
+                data.ChartData.forEach(opp => {
+                    chartData.push(opp.chartData);
+                    chartLabels.push(opp.chartLabel);
+                });
+            }
+
+            // Bar Chart
+            this.chartConfiguration3 = {
+                type: 'bar',
+                data: {
+                    labels: chartLabels,
+                    datasets: [
+                        {
+                            axis: 'y',
+                            label: data.barLabel,
+                            barPercentage: data.barPercentage,
+                            barThickness: data.barThickness,
+                            maxBarThickness: data.maxBarThickness,
+                            minBarLength: data.minBarLength,
+                            backgroundColor: data.backgroundColor,
+                            data: chartData,
+                        },
+                    ],
+                },
+                options: {
+                    plugins: {
+                       title: {
+                           display: false
+                       }
+                   },
+                   plugins: {
+                       legend: {
+                           display: false
+                       }
+                   },
+                    indexAxis: 'y',
+                    scales: {
+                        x: {
+                            grid: {
+                                display: false, // Hide x-axis lines
+                            },
+                            border: {
+                                display: false // remove border lines
+                            }
+                        },
+                        y: {
+                           grid: {
+                                display: false, // Hide y-axis lines
+                            },
+                            border: {
+                                display: false // remove border lines
+                            }
+                        },
+                    },
+
+                },
+            };
+
+            console.log('data => ', data);
+            this.error = undefined;
+        }
+    }
+
+    queryTerm;
+
+    handleKeyUp(evt) {
+        const isEnterKey = evt.keyCode === 13;
+        if (isEnterKey) {
+            this.queryTerm = evt.target.value;
         }
     }
 }
